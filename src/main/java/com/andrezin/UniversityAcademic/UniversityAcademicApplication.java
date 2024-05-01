fc5eb753daaee457059ef6e9e2eb5afff6f4a721
Musty <119085798+andrecamilis@users.noreply.github.com>
2024-05-01 17:46:02 -0300

    first commit

---
+++ src/main/java/com/andrezin/UniversityAcademic/UniversityAcademicApplication.java
@@ -0,0 +1,13 @@
+package com.andrezin.UniversityAcademic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityAcademicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityAcademicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrezin/UniversityAcademic/UniversityAcademicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrezin.UniversityAcademic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityAcademicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
